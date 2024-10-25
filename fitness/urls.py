7a3d7740820d6b91cf8c08729fc1d29024e5b524
Mohsen_Bernaoui <Mohsen.bernaoui@gmail.com>
2024-10-25 20:39:22 +0100

    Refactor navigation menu in home.html

--- fitness/urls.py
+++ fitness/urls.py
@@ -1,12 +1,24 @@
-# fitness/urls.py
-
 from django.urls import path
-from .views import exercise_list, exercise_add, exercise_edit, exercise_delete
 from . import views
+
 urlpatterns = [
-    path('', views.home, name='home'),  # Home page URL
-    path('exercises/', exercise_list, name='exercise_list'),
-    path('exercises/add/', exercise_add, name='exercise_add'),
-    path('exercises/edit/<int:exercise_id>/', exercise_edit, name='exercise_edit'),
-    path('exercises/delete/<int:exercise_id>/', exercise_delete, name='exercise_delete'),
+    # Exercise URLs
+    path('exercises/', views.exercise_list, name='exercise_list'),
+    path('exercises/add/', views.exercise_add, name='exercise_add'),
+    path('exercises/edit/<int:exercise_id>/', views.exercise_edit, name='exercise_edit'),
+    path('exercises/delete/<int:exercise_id>/', views.exercise_delete, name='exercise_delete'),
+
+    # Training Plan URLs
+    path('training_plans/', views.training_plan_list, name='training_plan_list'),
+    path('training_plans/add/', views.training_plan_add, name='training_plan_add'),
+    path('training_plans/edit/<int:plan_id>/', views.training_plan_edit, name='training_plan_edit'),
+    path('training_plans/delete/<int:id>/', views.training_plan_delete, name='training_plan_delete'),
+    path('training_plans/detail/<int:plan_id>/', views.training_plan_detail, name='training_plan_detail'),
+    # Exercise Set URLs
+    path('exercise_sets/add/<int:training_plan_id>/', views.exercise_set_add, name='exercise_set_add'),
+    path('exercise_sets/edit/<int:id>/', views.exercise_set_edit, name='exercise_set_edit'),
+    path('exercise_sets/delete/<int:id>/', views.exercise_set_delete, name='exercise_set_delete'),
+
+    # Home
+    path('', views.home, name='home'),
 ]
