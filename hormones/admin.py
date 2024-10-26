4740e7f24667df1611b9f168e688eeabf59cbfd6
farahzekri <farah.zekri@esprit.tn>
2024-10-26 19:36:18 +0200

    partie ia

--- hormones/admin.py
+++ hormones/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import HormoneLevel, HormoneRecommendation
+admin.site.register(HormoneLevel)
+admin.site.register(HormoneRecommendation)
